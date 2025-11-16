321edbfd2bf63817c543ee498c80043c85f7b372
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-11-16 19:32:59 +0530

    bug and fix

--- Pokemon Card/script.js
+++ Pokemon Card/script.js
@@ -1,19 +1,47 @@
-let pokeName = "pikachu="
-const PokeURL = `https://pokeapi.co/api/v2/pokemon/pikachu`
-let poke = document.querySelector('.poke')
-let PokeData = fetch(PokeURL)
-PokeData.then(response => response.json())
-.then((data) => {
-    console.log(data)
-    console.log("name: ",data.name)
-    console.log("weight: ",data.weight)
-    console.log("attack: ",data.moves[0].move.name)
-    console.log(data.stats[0].base_stat)
-    console.log(data.sprites.other.dream_world.front_default)
-    let imgSrc = data.sprites.other.home.front_default
-    poke.innerHTML = `
-    <img src="${imgSrc}">`
-}).catch((error)=> {
-    console.log(error,"pokemon not found!!");
-    
-})
+let searchBtn = document.getElementById("search-btn");
+let input = document.getElementById("pokemon-input");
+let result = document.querySelector(".result");
+
+searchBtn.addEventListener("click", () => {
+    let pokeName = input.value.toLowerCase().trim();
+    if (pokeName === "") {
+        result.innerHTML = `<h3 class="error">Please enter a Pokémon name!</h3>`;
+        return;
+    }
+
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            let imgSrc =
+                data.sprites.other.home.front_default ||
+                data.sprites.other.dream_world.front_default ||
+                data.sprites.front_default;
+
+            let types = data.types.map(t => t.type.name).join(", ");
+            let abilities = data.abilities.map(a => a.ability.name).join(", ");
+
+            result.innerHTML = `
+                <div class="poke-card">
+                    <h2>${data.name.toUpperCase()}</h2>
+                    <img src="${imgSrc}" alt="Pokemon Image">
+
+                    <p><b>Type:</b> ${types}</p>
+                    <p><b>Weight:</b> ${data.weight}</p>
+                    <p><b>Abilities:</b> ${abilities}</p>
+
+                    <h3>Stats</h3>
+                    <p>HP: ${data.stats[0].base_stat}</p>
+                    <p>Attack: ${data.stats[1].base_stat}</p>
+                    <p>Defense: ${data.stats[2].base_stat}</p>
+                    <p>Speed: ${data.stats[5].base_stat}</p>
+                </div>
+            `;
+        })
+        .catch(err => {
+            result.innerHTML = `<h2 class="error">Pokémon not found!</h2>`;
+        });
+});
